16f6159b765193f61f174a47e85a54f4835b99fc
Goliath33 <gauliath@msn.com>
2024-02-20 14:42:32 +0000

    Write a program that prints the alphabet in lowercase, followed by a new line

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,3 +6,17 @@
  * Putchar all alphabet nether 'qe'
  * Return: (Success)
  */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+		if (alpha != 'e' && alpha != 'q')
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
