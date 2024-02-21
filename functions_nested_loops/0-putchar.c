790b4175ea9b4e9a3eec91974cab51e9ddee8660
Goliath33 <gauliath@msn.com>
2024-02-21 14:40:51 +0000

    Write a program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - appel de putchar pour imprime _putchar
+ * putchar : imprime l'ensemble des arguments
+ * Return: (SUCCESS)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
