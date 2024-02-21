ef6df5e1e8956742c4587d45498fcd33b96fccdb
Goliath33 <gauliath@msn.com>
2024-02-21 14:44:46 +0000

    Write a program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stlib.h>
+
+/** 
+ * fichier de configuration + appel de librairie 
+ * avec les fonctions de rappelée
+ */
+
+int main(void);
+
+#endif
