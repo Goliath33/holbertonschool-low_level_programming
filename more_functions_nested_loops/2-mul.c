fac5b7c89adc77c54ddb54a0429bf26faa3562e5
Goliath33 <gauliath@msn.com>
2024-02-26 08:38:31 +0000

    Write a function that multiplies two integers

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _isdigit(int c) - verifie si un caractere est majuscule
- * @c: An input characte
- * Return: 1 if (SUCCESS) and 0 si pas de majuscule
+ * int mul(int a, int b) - verifie si un caractere est majuscule
+ * @a An input characte for multiplication
+ * @b ''     ''     ''      ''      ''
+ * Return: (SUCCESS)
  */
 
 int mul(int a, int b)
