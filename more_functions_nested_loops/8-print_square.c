e9f56d2a71c09b5bc505faf1aa3cc954b59cb083
Goliath33 <gauliath@msn.com>
2024-02-26 10:48:32 +0000

    Write a function that prints a square, followed by a new line

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_diagonal(int n) - affiche des # pour former un carre
+ * print_square - affiche des # pour former un carre
  * @h - give a height of character
  * @w - give a width of character
- * @size - An input give size of square
+ * @size: taille
  * Return: (SUCCESS)
  */
 
