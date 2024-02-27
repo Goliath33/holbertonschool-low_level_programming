bc4e45ec792a3b95ce52c654a2e267c8d28c8451
Goliath33 <gauliath@msn.com>
2024-02-27 10:20:00 +0000

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/** 
+ * fichier de configuration + appel de librairie 
+ * avec les fonctions de rappele
+ */
+
+int _abs(int);
+int main(void);
+int _isalpha(int c);
+int _isupper(int c);
+int _islower(int c);
+int _isdigit(int c);
+int _putchar(char c);
+int mul(int a, int b);
+int print_sign(int n);
+int print_last_digit(int);
+void more_numbers(void);
+void jack_bauer(void);
+void times_table(void);
+void print_line(int n);
+void print_numbers(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void print_most_numbers(void);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+void reset_to_98(int *n);
+
+#endif
