f758725f65ffb82021cd5d72f834b4a2cdf340f8
Goliath33 <gauliath@msn.com>
2024-02-27 13:29:57 +0000

    Write a function that prints a string, in reverse, followed by a new line

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /*
- * _puts: pousse un texte via "_puts"
- * Printf - utilise pour imprimer les strings (caractere) de str
- * @str - its pointers
+ * _puts: pousse un texte via "_putchar"
+ * *str - its pointers
  * Return: (SUCCESS)
  */
 
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * print_rev: pousse un texte via "_putchar" a l'envers
+ * *s - its pointers
+ * Return: (SUCCESS)
+ */
+
+void print_rev(char *s)
+{
+	int print = 0;
+	int rev = 0;
+
+	while (s[print] != '\0')
+	{
+		print++;
+	}
+	for (rev = print - 1; rev >= 0; rev--)
+	{
+		putchar(s[rev]);
+	}
+	putchar('\n');
+}
