3a34ebe1d52659e2e88166f29abd65f4a4e029f6
Goliath33 <gauliath@msn.com>
2024-03-02 13:40:40 +0000

    Write a function that concatenates two strings

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/** 
+ * fichier de configuration + appel de librairie 
+ * avec les fonctions de rappele
+ */
+
+int main(void);
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+#endif
