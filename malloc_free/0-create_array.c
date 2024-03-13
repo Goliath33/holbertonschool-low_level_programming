bd6d2ecabd61e6c026f8f350de158201a98c369f
Goliath33 <gauliath@msn.com>
2024-03-13 09:52:32 +0000

    modify 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,19 +7,21 @@
  * Return: Always NULL if size = 0.
  */
 
-#include <stdlib.h> // Include the necessary header for malloc
-
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i = 0;
+	char *s = malloc(size);
+
 	if (size == 0)
 		return (NULL);
 
-	char *s = malloc(size);
-
 	if (!s)
+	{
+		free(s);
 		return (NULL);
+	}
 
-	for (unsigned int i = 0; i < size; ++i)
+	for (; i < size; ++i)
 	{
 		s[i] = c;
 	}
