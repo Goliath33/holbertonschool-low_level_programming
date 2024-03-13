1f6bbac31e1ea9768b20e1e7644b2abc0536ef88
Goliath33 <gauliath@msn.com>
2024-03-13 10:21:41 +0000

    update the code 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @size - alloue un espace de memoire avec malloc
+ * @c - prend ma chaine de caractere
+ * Return: Always NULL if size = 0.
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_str = (char *)malloc((strlen(str) + 1) * sizeof(char));
+
+	if (new_str != NULL)
+	{
+		strcpy(new_str, str);
+	}
+
+	return (new_str);
+}
