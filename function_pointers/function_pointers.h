0f24f47db42be320ebb4f6c1256a578518245183
Goliath33 <gauliath@msn.com>
2024-03-20 09:17:30 +0000

    add 2-int_index.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <stddef.h>
 
 /** 
  * fichier de configuration + appel de librairie 
@@ -14,6 +15,7 @@
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 #endif
